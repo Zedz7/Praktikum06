6eb0eb45eef3685e7829daaf42a936640e2c84a6
Zaimz <hafizahmadzaimkuntoro@gmail.com>
2025-03-20 09:27:23 +0700

    Percobaan 2

---
+++ Mahasiswa06.java
@@ -0,0 +1,22 @@
+public class Mahasiswa06 {
+    String nim;
+    String nama;
+    String kelas;
+    double ipk;
+
+    public Mahasiswa06() {
+
+    }
+    public Mahasiswa06(String nm, String name, String kls, double ip) {
+        nim = nm;
+        nama = name;
+        kelas = kls;
+        ipk = ip;
+    }
+    void tampilInformasi() {
+        System.out.println("Nama    : " + nama);
+        System.out.println("NIM     : " + nim);
+        System.out.println("Kelas   : " + kelas);
+        System.out.println("IPK     : " + ipk);
+    }
+}
